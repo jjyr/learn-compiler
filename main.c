6ea9ac94ac2a7a9c3708994b3712e13c5c78f9fe
jjy <jjyruby@gmail.com>
2020-01-22 19:50:05 +0800

    partial evaluate fixnum

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "ast.h"
 #include "parser.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 // parse racket to ast
 ASTNode *parse_ast(char *source) {
@@ -9,10 +10,29 @@ ASTNode *parse_ast(char *source) {
   return parse_program(&p);
 }
 
-// partial evaluate root
-int partial_eval(ASTNode *root) {
-  // TODO
-  return 1;
+void partial_eval(ASTNode *node) {
+  switch (node->token) {
+  case Neg:
+    partial_eval(node->lhs);
+    if (node->lhs->token == Fixnum) {
+      node->token = Fixnum;
+      node->value = -node->lhs->value;
+      node->lhs = 0;
+    }
+    break;
+  case Add:
+    partial_eval(node->lhs);
+    partial_eval(node->rhs);
+    if (node->lhs->token == Fixnum && node->rhs->token == Fixnum) {
+      node->token = Fixnum;
+      node->value = node->lhs->value + node->rhs->value;
+      node->lhs = 0;
+      node->rhs = 0;
+    }
+    break;
+  default:
+    break;
+  }
 }
 
 void print_ast(ASTNode *node) {
@@ -36,14 +56,21 @@ void print_ast(ASTNode *node) {
     printf("(read)");
     break;
   default:
-    break;
+    printf("\nfailed to parse token %d\n", node->token);
+    exit(-1);
   }
 }
 
 int main() {
   char s[] = "(+ (read) (- (+ 5 3)))";
   ASTNode *root = parse_ast(s);
+  printf("inputs:\n");
   print_ast(root);
+  printf("\n");
+  printf("\n");
+  printf("partial eval:\n");
   partial_eval(root);
+  print_ast(root);
+  printf("\n");
   return 0;
 }
