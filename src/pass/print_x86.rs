60977365ecb9acddb959d0b38270be7e44a11864
jjy <jjyruby@gmail.com>
2020-05-28 20:57:38 +0800

    fix print_x86

--- src/pass/print_x86.rs
+++ src/pass/print_x86.rs
@@ -8,18 +8,18 @@ pub fn print_x86(f: &mut impl Write, node_list: Vec<Box<Node>>, call_info: CallI
 
     writeln!(f, ".global main")?;
     writeln!(f, "main:")?;
-    writeln!(f, "PUSHQ %%rbp")?;
-    writeln!(f, "MOVQ %%rsp, %%rbp")?;
-    writeln!(f, "SUBQ ${}, %%rsp", call_info.vars_count * WORD)?;
+    writeln!(f, "PUSHQ %rbp")?;
+    writeln!(f, "MOVQ %rsp, %rbp")?;
+    writeln!(f, "SUBQ ${}, %rsp", call_info.vars_count * WORD)?;
 
     for node in node_list {
         let Node { token, value } = *node;
         match value {
             MOVQ { target, source } => {
-                writeln!(f, "MOVQ {}, {}", parse_val(target), parse_val(source))?;
+                writeln!(f, "MOVQ {}, {}", parse_val(source), parse_val(target))?;
             }
             ADDQ { target, arg } => {
-                writeln!(f, "ADDQ {}, {}", parse_val(target), parse_val(arg))?;
+                writeln!(f, "ADDQ {}, {}", parse_val(arg), parse_val(target))?;
             }
             CALLQ(symbol) => {
                 writeln!(f, "CALLQ {}", symbol)?;
@@ -31,12 +31,12 @@ pub fn print_x86(f: &mut impl Write, node_list: Vec<Box<Node>>, call_info: CallI
     }
 
     // print out return value
-    writeln!(f, "MOVQ %%rax, %%rdi")?;
+    writeln!(f, "MOVQ %rax, %rdi")?;
     writeln!(f, "CALLQ print_int")?;
     // resume the stack and return 0
-    writeln!(f, "ADDQ ${}, %%rsp", call_info.vars_count * WORD)?;
-    writeln!(f, "MOVQ $0, %%rax")?;
-    writeln!(f, "POPQ %%rbp")?;
+    writeln!(f, "ADDQ ${}, %rsp", call_info.vars_count * WORD)?;
+    writeln!(f, "MOVQ $0, %rax")?;
+    writeln!(f, "POPQ %rbp")?;
     writeln!(f, "retq")?;
     Ok(())
 }
@@ -47,9 +47,9 @@ fn parse_val(node: Box<Node>) -> String {
     let Node { token: _, value } = *node;
 
     match value {
-        Fixnum(n) => format!("{}", n),
-        StackLoc(offset) => format!("{}(%%rbp)", offset),
-        RAX => "%%rax".to_string(),
+        Fixnum(n) => format!("${}", n),
+        StackLoc(offset) => format!("{}(%rbp)", offset),
+        RAX => "%rax".to_string(),
         value => {
             panic!("failed to parse node {:?}", value);
         }
